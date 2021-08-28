6e6dc2a49febd1316334b0493a8c9ed50bed5acd
Jakub Maćkowiak <65769532+JakubMckowiak@users.noreply.github.com>
2021-08-28 20:20:12 +0200

    Updated UI to an early state, in which it'll stay for the time of being.

--- Library/Views/PlantView.xaml.cs
+++ Library/Views/PlantView.xaml.cs
@@ -24,5 +24,10 @@ namespace CarePlant.Views
         {
             InitializeComponent();
         }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
     }
 }
